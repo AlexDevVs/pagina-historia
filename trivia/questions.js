9df81c186b00de5fed11e80962763c4df68931b4
AlexDevVs <147619958+AlexDevVs@users.noreply.github.com>
2023-11-15 09:37:13 -0300

    Add files via upload

---
+++ trivia/questions.js
@@ -0,0 +1,59 @@
+// This is the question funstions that contain questions and the answers. They are in multidimensional array with inner array elements
+var questions = [
+  { 
+      question: "¿Qué es la Burguesia?", 
+      answers: [
+          { text: "Un clase social", correct: true },
+          { text: "Una forma de gobierno" , correct: false },
+          { text: "Una comida", correct: false }
+      ]
+  },
+  { 
+      question: "¿Por qué se realizan las revoluciones?", 
+      answers: [
+          { text: "Para representar un cambio profundo", correct: true },
+          { text: "Para aumentar la desigualdad social", correct: false },
+          { text: "Una forma de resolver disputas pacificamente", correct: false }
+      ]
+  },
+  { 
+      question: "¿Qué es el Despotismo?", 
+      answers: [
+          { text: "Una Dictadura", correct: false },
+          { text: "Una Democracia", correct: true },
+          { text: "Una Religión", correct: false }
+      ]
+  },
+  { 
+      question: "¿Para qué fue fundado el Ku Klux Klan?", 
+      answers: [
+          { text: "Oprimir a los afroamericanos", correct: true },
+          { text: "Aalabar a un Dios", correct: false },
+          { text: "Ayudar a los afroamericanos", correct: false }
+      ]
+  },
+  { 
+      question: "¿De qué mandera logró sus objetivos el Ku Klux Clan?",
+      answers: [
+          { text: "Hablando pacificamente", correct: false },
+          { text: "Mediante la promoción de diversidad y la igualdad", correct: false },
+          { text: "Violencia extrema", correct: true }
+      ]
+  },
+  { 
+    question: "¿Qupe hizo la Burguesía durante la Revolución Industrial?",
+    answers: [
+        { text: "Invirtieron en fábricas y maquinarias", correct: true },
+        { text: "No hicieron nada", correct: false },
+        { text: "Violencia extrema", correct: true }
+    ]
+  },
+  { 
+    question: "¿En que año sucedió la Doble Revolución?",
+    answers: [
+        { text: "Siglo XII", correct: false },
+        { text: "Siglo XIX", correct: true },
+        { text: "Siglo XVI", correct: false }
+    ]
+  },
+];
